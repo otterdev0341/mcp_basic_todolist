cf29e50d72767391aa59f30d2b098c481b7a1501
Otterdev_0341 <128258559+otterdev0341@users.noreply.github.com>
2025-05-09 17:10:46 +0700

    impl serverHandler for mcp handler

--- mcp_todolist/src/infrastructure/mcp_handler/handler.rs
+++ mcp_todolist/src/infrastructure/mcp_handler/handler.rs
@@ -1,7 +1,8 @@
 
 
 use std::sync::Arc;
-use rmcp::{model::{AnnotateAble, CallToolResult, Content, RawResource, Resource}, tool, Error as McpError};
+use rmcp::{const_string, model::{AnnotateAble, CallToolResult, ConstString, Content, GetPromptRequestParam, GetPromptResult, Implementation, ListPromptsResult, ListResourceTemplatesResult, ListResourcesResult, PaginatedRequestParam, Prompt, PromptArgument, PromptMessage, PromptMessageContent, PromptMessageRole, ProtocolVersion, RawResource, ReadResourceRequestParam, ReadResourceResult, Resource, ResourceContents, ServerCapabilities, ServerInfo}, service::RequestContext, tool, Error as McpError, RoleServer, ServerHandler};
+use serde_json::json;
 use crate::{application::usecase::todo_usecase::TodolistUseCase, domain::dto::todo_dto::{ReqCreateTodoDto, GetTaskById, ReqUpdateTodoDto}};
 
 #[allow(dead_code)]
@@ -21,6 +22,9 @@ impl MCPHandler {
     fn _create_resource_text(&self, uri: &str, name: &str) -> Resource {
         RawResource::new(uri, name.to_string()).no_annotation()
     }
+
+    
+
 
     #[tool(description = r#"
         ใช้คำสั่งนี้เพื่อสร้างงานใหม่ในระบบ / Use this to create a new task.
@@ -261,4 +265,126 @@ impl MCPHandler {
             Err(e) => Err(McpError::internal_error(e.to_string(), None))
         }
     }
+}
+
+
+
+
+
+
+const_string!(Echo = "echo");
+#[tool(tool_box)]
+impl ServerHandler for MCPHandler {
+    
+    fn get_info(&self) -> ServerInfo {
+        ServerInfo {
+            protocol_version: ProtocolVersion::V_2024_11_05,
+            capabilities: ServerCapabilities::builder()
+                .enable_prompts()
+                .enable_resources()
+                .enable_tools()
+                .build(),
+            server_info: Implementation::from_build_env(),
+            instructions: Some("Real personal financial analysis".to_string()),
+        }
+    }
+
+    async fn list_resources(
+        &self,
+        _request: Option<PaginatedRequestParam>,
+        _: RequestContext<RoleServer>,
+    ) -> Result<ListResourcesResult, McpError> {
+        Ok(ListResourcesResult {
+            resources: vec![
+                self._create_resource_text("str:////Users/to/some/path/", "cwd"),
+                self._create_resource_text("memo://insights", "memo-name"),
+            ],
+            next_cursor: None,
+        })
+    }
+
+    async fn read_resource(
+        &self,
+        ReadResourceRequestParam { uri }: ReadResourceRequestParam,
+        _: RequestContext<RoleServer>,
+    ) -> Result<ReadResourceResult, McpError> {
+        match uri.as_str() {
+            "str:////Users/to/some/path/" => {
+                let cwd = "/Users/to/some/path/";
+                Ok(ReadResourceResult {
+                    contents: vec![ResourceContents::text(cwd, uri)],
+                })
+            }
+            "memo://insights" => {
+                let memo = "Business Intelligence Memo\n\nAnalysis has revealed 5 key insights ...";
+                Ok(ReadResourceResult {
+                    contents: vec![ResourceContents::text(memo, uri)],
+                })
+            }
+            _ => Err(McpError::resource_not_found(
+                "resource_not_found",
+                Some(json!({
+                    "uri": uri
+                })),
+            )),
+        }
+    }
+
+    async fn list_prompts(
+        &self,
+        _request: Option<PaginatedRequestParam>,
+        _: RequestContext<RoleServer>,
+    ) -> Result<ListPromptsResult, McpError> {
+        Ok(ListPromptsResult {
+            next_cursor: None,
+            prompts: vec![Prompt::new(
+                "example_prompt",
+                Some("This is an example prompt that takes one required argument, message"),
+                Some(vec![PromptArgument {
+                    name: "message".to_string(),
+                    description: Some("A message to put in the prompt".to_string()),
+                    required: Some(true),
+                }]),
+            )],
+        })
+    }
+
+    async fn get_prompt(
+        &self,
+        GetPromptRequestParam { name, arguments }: GetPromptRequestParam,
+        _: RequestContext<RoleServer>,
+    ) -> Result<GetPromptResult, McpError> {
+        match name.as_str() {
+            "example_prompt" => {
+                let message = arguments
+                    .and_then(|json| json.get("message")?.as_str().map(|s| s.to_string()))
+                    .ok_or_else(|| {
+                        McpError::invalid_params("No message provided to example_prompt", None)
+                    })?;
+
+                let prompt =
+                    format!("This is an example prompt with your message here: '{message}'");
+                Ok(GetPromptResult {
+                    description: None,
+                    messages: vec![PromptMessage {
+                        role: PromptMessageRole::User,
+                        content: PromptMessageContent::text(prompt),
+                    }],
+                })
+            }
+            _ => Err(McpError::invalid_params("prompt not found", None)),
+        }
+    }
+
+    async fn list_resource_templates(
+        &self,
+        _request: Option<PaginatedRequestParam>,
+        _: RequestContext<RoleServer>,
+    ) -> Result<ListResourceTemplatesResult, McpError> {
+        Ok(ListResourceTemplatesResult {
+            next_cursor: None,
+            resource_templates: Vec::new(),
+        })
+    }
+
 }
--- mcp_todolist/src/main.rs
+++ mcp_todolist/src/main.rs
@@ -1,5 +1,42 @@
+use std::sync::Arc;
+
+use anyhow::Result;
+use mcp_todolist::{application::usecase::todo_usecase::TodolistUseCase, configuration::config, infrastructure::{mcp_handler::handler::MCPHandler, sqlite::{db_connection::sqlite_con::conn, repository_impl::todolist::TodoListSqliteRepository}}};
+use rmcp::{ transport::stdio, ServiceExt};
+use tracing_subscriber::EnvFilter;
 
 #[tokio::main]
-async fn main() {
-    println!("Hello, world!");
+async fn main() -> Result<()> {
+    
+    
+    let config = config::load()?;
+
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env().add_directive(tracing::Level::INFO.into()))
+        .with_writer(std::io::stderr)
+        .with_ansi(false)
+        .init();
+
+    tracing::info!("Start the application");
+
+    let db_poll = conn(&config.database_url)?;
+    let db_pool_arc = Arc::new(db_poll);
+    
+    let todo_use_case = {
+        let todo_repo = TodoListSqliteRepository::new(Arc::clone(&db_pool_arc));
+        TodolistUseCase::new(Arc::new(todo_repo))
+    };
+
+    let service = MCPHandler::new(
+        Arc::new(todo_use_case)
+    ).serve(stdio())
+    .await
+    .inspect_err(|e|{
+        tracing::error!("serving error: {:?}", e);
+    })?;
+
+    service.waiting().await?;
+    
+
+    Ok(())
 }
